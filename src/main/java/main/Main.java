827d323a1cb6a5c592965555cf12ae32a1975cc1
mihairusu05 <mihai10rusu+github@gmail.com>
2025-12-10 21:42:47 +0200

    Finished KNN model

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,23 +1,37 @@
 package main;
 
+import core.Instance;
 import data.CSVReader;
 import data.DataPreprocessor;
 import data.DataSplitter;
+import models.KNN;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args){
-        CSVReader reader = new CSVReader("D:\\Info\\School\\Java\\java-binary-classifier\\data\\cancer.csv");
-        System.out.println(reader.getData());
-        System.out.println("**********************");
-        DataPreprocessor preProcessor = new DataPreprocessor(reader.getData());
+        CSVReader reader1 = new CSVReader("D:\\Info\\School\\Java\\java-binary-classifier\\data\\cancer.csv");
+        CSVReader reader2 = new CSVReader("D:\\Info\\School\\Java\\java-binary-classifier\\data\\sonar data.csv");
+        CSVReader reader3 = new CSVReader("D:\\Info\\School\\Java\\java-binary-classifier\\data\\telescope_data_no_id.csv");
+        DataPreprocessor preProcessor = new DataPreprocessor(reader3.getData());
         preProcessor.process();
-        System.out.println(preProcessor.getData());
-        System.out.println("**********************");
         DataSplitter dataSplitter = new DataSplitter(preProcessor.getData());
         dataSplitter.splitData(0.8);
-        System.out.println(dataSplitter.getTrainData());
-        System.out.println("**********************");
-        System.out.println(dataSplitter.getTestData());
-        System.out.println("**********************");
-    }
+        List<Instance<Double, String>> trainData = dataSplitter.getTrainData();
+        List<Instance<Double, String>> testData = dataSplitter.getTestData();
+        List<String> label = new ArrayList<>();
+        testData.forEach(p -> label.add(p.getOutput()));
+
+        KNN knnModel = new KNN(61);
+        knnModel.train(trainData);
+        List<String> predictions = knnModel.test(testData);
+        double correct = 0;
+        for (int m = 0; m< label.size() ; m++)
+            if (predictions.get(m).equals(label.get(m))){
+                correct++;
+            }
+        System.out.println("For k = " + 61 + "the accuracy is : " + correct*100/label.size() + " %");
+       }
+
 }
---
+++ src/main/java/models/KNN.java
@@ -0,0 +1,93 @@
+package models;
+
+import core.Instance;
+import core.Model;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class KNN implements Model<Double, String> {
+    /// For Knn training the best k should be between 1 and rad(number of training points)
+
+    private Integer k;
+    private List<Instance<Double, String>> trainingData;
+
+    public KNN(int k){
+        if ( k < 1){
+            throw new IllegalArgumentException("K parameter should be bigger than 0");
+        }
+        this.k = k;
+    }
+
+    public void setK(Integer k) {
+        this.k = k;
+    }
+
+    @Override
+    public void train(List<Instance<Double, String>> trainingData){
+        // Store reference directly or copy once.
+        // No need to copy inside the test loop.
+        this.trainingData = trainingData;
+    }
+
+    private double distanceEuclidean(List<Double> e1, List<Double> e2){
+        double sum = 0.0;
+        int size = e1.size();
+        for (int i = 0; i < size; i++){
+            double diff = e1.get(i) - e2.get(i);
+            sum += diff * diff;
+        }
+        return Math.sqrt(sum);
+    }
+
+    @Override
+    public List<String> test(List<Instance<Double, String>> testData){
+        return testData.stream()
+                .map(this::findLabel)
+                .collect(Collectors.toList());
+    }
+
+    public String findLabel(Instance<Double, String> instance){
+        class NeighborPair implements Comparable<NeighborPair> {
+            Double distance;
+            String label;
+
+            public NeighborPair(Double d, String l) {
+                this.distance = d;
+                this.label = l; }
+
+            @Override
+            public int compareTo(NeighborPair other) {
+                return this.distance.compareTo(other.distance);
+            }
+        }
+
+        List<NeighborPair> neighbors = new ArrayList<>();
+        for (Instance<Double, String> trainInstance : this.trainingData) {
+            double dist = distanceEuclidean(trainInstance.getInput(), instance.getInput());
+            neighbors.add(new NeighborPair(dist, trainInstance.getOutput()));
+        }
+        Collections.sort(neighbors);
+        List<String> topKLabels = new ArrayList<>();
+        for (int i = 0; i < Math.min(this.k, neighbors.size()); i++) {
+            topKLabels.add(neighbors.get(i).label);
+        }
+
+        Map<String, Integer> counts = new HashMap<>();
+        for (String label : topKLabels) {
+            counts.put(label, counts.getOrDefault(label, 0) + 1);
+        }
+
+        String bestLabel = "";
+        int maxCount = -1;
+
+        for (Map.Entry<String, Integer> entry : counts.entrySet()) {
+            if (entry.getValue() > maxCount) {
+                maxCount = entry.getValue();
+                bestLabel = entry.getKey();
+            }
+        }
+
+        return bestLabel;
+    }
+}
